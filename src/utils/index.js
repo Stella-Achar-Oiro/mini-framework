bc48c77dd80314df4448c05e68cdc47a40271e70
steoiro <stellaoiro11@gmail.com>
2025-06-26 21:06:00 +0300

    Refactor utility module exports for improved organization and clarity

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,9 @@
 /**
  * Utility Modules
+ * @module utils
  */
 
 export * from './helpers.js';
 export { Logger } from './logger.js';
+export { ErrorBoundary, ERROR_TYPES, ERROR_SEVERITY } from './error-boundary.js';
+export * from './dom-helpers.js';
